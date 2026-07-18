e65b4552c069a41598064962147add12a7499f78
rim <takie20m2018@gmail.com>
2026-07-18 12:46:09 -0600

    fixed

--- emoji.py
+++ emoji.py
@@ -10,6 +10,29 @@ dificullty_leveels = {
     "Hard": [
         "🐶", "😺", "🐭", "🐹"
         "🦊", "🐻", "🐼", "🐸"
-        "🐵", "", "", ""
+        "🐵", "🦁", "🐯", "🐨"
     ]     
 }
+
+
+class EmojiMemoryGame:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.title("🧠 Emoji Memory Match")
+        self.root.geometry("600x650")
+
+        self.difficulty = tk.StringVar(value="Easy")
+
+        self.create_menu()
+
+    def create_menu(self):
+
+        tk.Label(
+            self.root,
+            text="🧠 Emoji Memory match",
+            font=("Arial", 24)
+        ).pack(pady=20)
+        
+        tk.Label(self.root,
+                 )
