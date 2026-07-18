4b0d4ae29da3420ff03956aad9d9bf76a0ff57d5
rim <takie20m2018@gmail.com>
2026-07-18 13:07:36 -0600

    Fourth

--- emoji.py
+++ emoji.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import random
 
-dificullty_leveels = {
+dificullty_levels = {
     "Easy": ["🐶", "😺", "🐭", "🐹"],
     "Medium": [
         "🐶", "😺", "🐭", "🐹"
@@ -28,6 +28,7 @@ class EmojiMemoryGame:
 
     def create_menu(self):
 
+        
         tk.Label(
             self.root,
             text="🧠 Emoji Memory match",
@@ -35,4 +36,31 @@ class EmojiMemoryGame:
         ).pack(pady=20)
         
         tk.Label(self.root,
-                 )
+                text="Choose difficulty",
+                font=("Arial", 16)
+        ).pack()
+
+        for level in difficulty_levels:
+            tk.Radiobutton(
+                self.root,
+                text=level,
+                variable=self.difficulty,
+                value=level,
+                font=("Arial", 14)
+            ).pack()
+
+        tk.button(
+            self.root,
+            text="🚀 Start game",
+            font=("Arial", 16),
+            command=self.start_game
+        ).pack(pady=20)
+
+
+    def start_game (self):
+
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+        self.moves = 0
+        self.matches
